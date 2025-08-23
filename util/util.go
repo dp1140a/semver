740e7dadf7f4f41ef01ff0097ab574a0e3ea1228
dp1140a <dave@mymobile.info>
2025-08-23 16:11:43 -0700

    - Refactor: Simplify `ValidVersionString` by using `regexp.MustCompile` - Add `CleanVersion` function to handle trailing/leading whitespace in version strings - Remove unused functions: `WriteVersionFile`, `VersionFileExists`, and `InGitDir`

--- util/util.go
+++ util/util.go
@@ -1,10 +1,8 @@
 package util
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
 	"regexp"
+	"strings"
 )
 
 var NOT_GIT_MSG = `The current directory is not a git project.
@@ -13,41 +11,14 @@ $ git init`
 
 const SemVerRegex = `^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-\.]+(?:\.[0-9a-zA-Z-]+)*))?$`
 
-/**
-const SemVerRegex = `^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(
-?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$`
-**/
-/*
- */
+var semverRe = regexp.MustCompile(SemVerRegex)
+
+// CleanVersion trims trailing/leading whitespace (incl. \r\n)
+func CleanVersion(s string) string {
+	return strings.TrimSpace(s)
+}
+
 func ValidVersionString(version string) bool {
-	r, _ := regexp.Compile(SemVerRegex)
-	return r.MatchString(version)
-}
-
-func WriteVersionFile(version string) error {
-	fmt.Println("Writing VERSION file")
-	err := os.WriteFile("VERSION", []byte(version), 0644)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func VersionFileExists(cwd string) bool {
-	_, err := os.Stat("VERSION")
-	if err != nil {
-		return false
-	} else {
-		return true
-	}
-}
-
-func InGitDir(cwd string) {
-	//Look for .git directory.  If not exit
-	if _, err := os.Stat(filepath.Join(cwd, ".git")); err != nil {
-		if os.IsNotExist(err) {
-			fmt.Println(NOT_GIT_MSG)
-			os.Exit(0)
-		}
-	}
+	version = CleanVersion(version) // <-- ignore trailing newline/CR/spaces
+	return semverRe.MatchString(version)
 }
